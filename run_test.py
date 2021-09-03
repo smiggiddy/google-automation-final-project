9fee9dba19e8297db08ee5f7290d5080b290fc1a
Smig <smig@smiggiddy.com>
2021-09-03 14:56:28 -0400

    program will test the functions in the run script

---
+++ run_test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+tests if the run program works and reads the files
+for the absother python functions needed by program.
+"""
+
+import unittest
+from run import list_directory_files
